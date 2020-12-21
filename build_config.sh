8ba82379ed4d62581052fc0ce38a7287908e5463
zhaoyubin <zyb@palliums.org>
2020-12-21 15:10:45 +0800

    修改bug

--- build_config.sh
+++ build_config.sh
@@ -55,7 +55,7 @@ fi
 read  -p "Please Enter The Number of Nodes Created:" num
 read  -p "Please Enter The Primary Node IP:" master_node_ip
 read  -p "Please Enter All Deployed IP,Separated by \",\":" deployed_ip
-read  -p "Please Enter num-full-nodes:" num-full-nodes
+read  -p "Please Enter num-full-nodes:" num_full_nodes
 # randseed=`$config_dir_path/randseed`
 # echo $randseed >$config_dir_path/config/seed
 # $HOME/violas/target/release/config-builder faucet -o $config_dir_path/config -s $randseed -n $num
@@ -110,12 +110,12 @@ do
 done
 
 cd $HOME/violas/target/release/
-if [ $num-full-nodes eq 0]; then
+if [ $num-full-nodes eq 0 ]; then
 	nohup $HOME/violas/target/release/diem-swarm -c $HOME/violascfg --diem-node $HOME/violas/target/release/diem-node -n $num >$config_dir_path/swarm.log 2>&1 &
 	sleep 5
 	killall diem-node
 else
-	nohup $HOME/violas/target/release/diem-swarm -c $HOME/violascfg --diem-node $HOME/violas/target/release/diem-node -n $num -f $num-full-nodes>$config_dir_path/swarm.log 2>&1 &
+	nohup $HOME/violas/target/release/diem-swarm -c $HOME/violascfg --diem-node $HOME/violas/target/release/diem-node -n $num -f $num_full_nodes >$config_dir_path/swarm.log 2>&1 &
 	sleep 10
 	killall diem-node
 fi
